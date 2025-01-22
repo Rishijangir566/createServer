396fd6bee4e025c9d429b9e1ce55993d223ecff0
Rishijangir566 <rishijangirjr@gmail.com>
2025-01-22 17:25:48 +0530

    use postman get , post, put , delete

--- server.js
+++ server.js
@@ -13,13 +13,38 @@ const server = http.createServer((request, response) => {
     response.writeHead(200,{"Content-Type":"appliaction/json"})
     response.end(JSON.stringify(cars))
   } 
-//   else if (request.method === "POST") {
-    
-//   } else if (request.method === "DELETE" && request.url.match(/\/\d+/)) {
-    
-//   } else if (request.method === "PUT" && request.url.match(/\/\d+/)) {
-    
-// }
+  else if (request.method === "POST") {
+      let body =""
+      request.on("data" , (item)=>{
+        body+=item
+      });
+      request.on("end",()=>{
+        cars.push(JSON.parse(body));
+        response.writeHead(201,{"Content-Type":"appliaction/json"})
+        response.end(JSON.stringify(cars))
+      })
+
+  } 
+  else if (request.method === "DELETE" && request.url.match(/\/\d+/)) {
+    const idDelete =request.url.split("/")[1]
+    const updatedata=cars.filter((caritem)=>caritem.id!==Number(idDelete))
+    response.writeHead(200,{"Content-Type":"appliaction/json"})
+    response.end(JSON.stringify(updatedata))
+} 
+else if (request.method === "PUT" && request.url.match(/\/\d+/)) {
+      const idEdit =request.url.split("/")[1]
+     let body =""
+     request.on("data",(items)=>{
+        body+=items
+     })
+     request.on("end", ()=>{
+        const updateData =cars.map((caritem)=>{
+            return caritem.id===Number(idEdit)?JSON.parse(body):caritem
+        })
+        response.writeHead(200,{"Content-Type":"appliaction/json"})
+    response.end(JSON.stringify(updateData))
+     })
+}
 });
    
 
